cd49f3f64faa04c6e35509ecabd1d631a8b5ccfa
gafmgafm <greg@gregmartins.me>
2018-05-26 20:02:46 +1000

    Added condition type to database

--- tools/data/ddl/condition_table.sql
+++ tools/data/ddl/condition_table.sql
@@ -1,8 +1,12 @@
---id,name,date_created,date_modified
+--id,name,condition_type_id,date_created,date_modified
 
 CREATE TABLE condition (
     id INTEGER PRIMARY KEY,
     name VARCHAR NOT NULL,
+    condition_type_id CHAR(1) NOT NULL,
     date_created VARCHAR DEFAULT (datetime('now')) NOT NULL,
-    date_modified VARCHAR DEFAULT (datetime('now')) NOT NULL
+    date_modified VARCHAR DEFAULT (datetime('now')) NOT NULL,
+    FOREIGN KEY (condition_type_id) REFERENCES condition_type(id)
 )
+;
+CREATE INDEX c_01_ix ON condition (condition_type_id)
