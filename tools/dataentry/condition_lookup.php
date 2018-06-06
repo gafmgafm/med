87d8dcb4598c67887cabee63e83f00c603fe91d3
gafmgafm <greg@gregmartins.me>
2018-06-06 20:40:31 +1000

    Added alias to lookup

--- tools/dataentry/condition_lookup.php
+++ tools/dataentry/condition_lookup.php
@@ -1,10 +1,14 @@
 <?php
 require_once(__DIR__.'/../lib/lib.php');
 require_once('dao/condition_dao.php');
+require_once('dao/condition_aka_dao.php');
 
 $term = strtolower($_GET['term']);
-$data = ConditionDAO::lookup($term);
+$conditions = ConditionDAO::lookup($term);
+$alias = ConditionAkaDAO::searchName($term);
 
-echo json_encode($data);
+
+
+echo json_encode(array_merge($conditions, $alias));
 
 ?>
