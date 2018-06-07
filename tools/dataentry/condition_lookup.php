524ff0fed541d5db9c08b96b2bc6042c7a77331b
gafmgafm <greg@gregmartins.me>
2018-06-07 19:59:06 +1000

    Formatting

--- tools/dataentry/condition_lookup.php
+++ tools/dataentry/condition_lookup.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once(__DIR__.'/../lib/lib.php');
 require_once('dao/condition_dao.php');
 require_once('dao/condition_aka_dao.php');
@@ -6,8 +7,6 @@ require_once('dao/condition_aka_dao.php');
 $term = strtolower($_GET['term']);
 $conditions = ConditionDAO::lookup($term);
 $alias = ConditionAkaDAO::searchName($term);
-
-
 
 echo json_encode(array_merge($conditions, $alias));
 
